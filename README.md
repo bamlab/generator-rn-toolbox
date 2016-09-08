e667f02410f34030b3851c56b3e6ca2902050da5
Yann Leflour <yannl@bamlab.fr>
2016-09-09 01:15:27 +0200

    fix fastlane

---
+++ README.md
@@ -0,0 +1,10 @@
+# RN Toolbox
+Yeoman generators to kickstart your react-native project
+
+## Warning
+The way those generators are configured is heavily biased based on my company's own needs.
+
+## Generators
+- `yo rn-toolbox:eslint` to setup airbnb react native eslint
+- `yo rn-toolbox:jest` to setup jest tests
+- `yo rn-toolbox:fastlane` to setup fastlane for one line deploy to Hockey App
--- generators/fastlane/index.js
+++ generators/fastlane/index.js
@@ -16,41 +16,79 @@ module.exports = generators.Base.extend({
     },
     {
       type    : 'input',
+      name    : 'keystorePassword',
+      message : 'Your keystore password',
+      default : '*********',
+    },
+    {
+      type    : 'input',
       name    : 'hockeyAppToken',
       message : 'A valid HockeyApp token'
     }]).then(function (answers) {
       this.log('app id', answers.appId);
       this.answers = answers;
+      this.answers.lowerCaseAppName = answers.appName.toLowerCase();
       this.log('HockeyApp token', answers.hockeyAppToken);
     }.bind(this));
   },
-  writing: function () {
-    this.fs.copyTpl(
-      this.templatePath('.env.dist'),
-      this.destinationPath('fastlane/.env.dist'),
-      this.answers
-    );
-    this.fs.copyTpl(
-      this.templatePath('.env'),
-      this.destinationPath('fastlane/.env'),
-      this.answers
-    );
-    this.fs.copyTpl(
-      this.templatePath('Fastfile'),
-      this.destinationPath('fastlane/Fastfile')
-    );
-    this.fs.copyTpl(
-      this.templatePath('Gymfile'),
-      this.destinationPath('fastlane/Gymfile'),
-      this.answers
-    );
-    this.fs.copyTpl(
-      this.templatePath('Matchfile'),
-      this.destinationPath('fastlane/Matchfile')
-    );
-    this.fs.copyTpl(
-      this.templatePath('Appfile'),
-      this.destinationPath('fastlane/Appfile')
-    );
-  }
+  writing: {
+    copy: function () {
+      this.fs.copyTpl(
+        this.templatePath('.env.dist'),
+        this.destinationPath('fastlane/.env.dist'),
+        this.answers
+      );
+      this.fs.copyTpl(
+        this.templatePath('.env'),
+        this.destinationPath('fastlane/.env'),
+        this.answers
+      );
+      this.fs.copyTpl(
+        this.templatePath('Fastfile'),
+        this.destinationPath('fastlane/Fastfile')
+      );
+      this.fs.copyTpl(
+        this.templatePath('Gymfile'),
+        this.destinationPath('fastlane/Gymfile'),
+        this.answers
+      );
+      this.fs.copyTpl(
+        this.templatePath('Matchfile'),
+        this.destinationPath('fastlane/Matchfile')
+      );
+      this.fs.copyTpl(
+        this.templatePath('Appfile'),
+        this.destinationPath('fastlane/Appfile')
+      );
+      this.fs.copyTpl(
+        this.templatePath('build.gradle'),
+        this.destinationPath('android/app/build.gradle')
+      );
+    },
+    extendGitignore: function() {
+      var content = this.fs.read(this.destinationPath('.gitignore'))
+      this.fs.copyTpl(
+        this.templatePath('.gitignore'),
+        this.destinationPath('.gitignore'),
+        { content: content }
+      );
+    },
+    createKeystore: function() {
+      var path = 'android/app/' + this.answers.lowerCaseAppName + '.keystore';
+      if(!this.fs.exists(this.destinationPath(path))) {
+        this.spawnCommand('keytool', [
+          '-genkey',
+          '-v',
+          '-dname', 'OU=BAM',
+          '-keystore', path,
+          '-alias', this.answers.lowerCaseAppName,
+          '-keyalg', 'RSA',
+          '-keysize', '2048',
+          '-validity', '10000',
+          '-storepass', this.answers.keystorePassword,
+          '-keypass', this.answers.keystorePassword,
+        ]);
+      }
+    },
+  },
 });
