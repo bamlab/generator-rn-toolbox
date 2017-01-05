d35153114d74e7d9ac3dcf80726b4ef126eca4c0
maximesraiki <maximes@theodo.fr>
2017-01-05 15:05:12 +0100

    add circle generator

---
+++ generators/circle/index.js
@@ -0,0 +1,16 @@
+const Base = require('yeoman-generator').Base;
+
+class CircleGenerator extends Base {
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath('circle.yml'),
+      this.destinationPath('circle.yml')
+    );
+  };
+  end() {
+    this.config.set('circle', true);
+    this.config.save();
+  };
+}
+
+module.exports = CircleGenerator;
