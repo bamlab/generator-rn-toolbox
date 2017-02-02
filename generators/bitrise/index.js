768c065d6f78c8e392075d571d541c4c1bbf31fc
Yann Leflour <yannl@bamlab.fr>
2017-02-02 18:23:08 +0100

    [Bitrise] Update Bitrise pipe

--- generators/bitrise/index.js
+++ generators/bitrise/index.js
@@ -21,18 +21,8 @@ class BitriseGenerator extends Base {
 
   writing() {
     this.fs.copyTpl(
-      this.templatePath('bitrise.common.yml'),
-      this.destinationPath('bitrise/bitrise.common.yml'),
-      this.answers
-    );
-    this.fs.copyTpl(
-      this.templatePath('bitrise.android.yml'),
-      this.destinationPath('bitrise/bitrise.android.yml'),
-      this.answers
-    );
-    this.fs.copyTpl(
-      this.templatePath('bitrise.ios.yml'),
-      this.destinationPath('bitrise/bitrise.ios.yml'),
+      this.templatePath('bitrise.yml'),
+      this.destinationPath('bitrise.yml'),
       this.answers
     );
   }
